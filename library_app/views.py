b94ce9dbee29cf1fdfccf756ca8a183a88823cb5
Nati <netanelzelzermn@gmail.com>
2023-01-05 21:09:27 +0200

    test commit to test sapir-repo-merge branch

--- library_app/views.py
+++ library_app/views.py
@@ -1,6 +1,6 @@
 # from django.http import HttpResponseRedirect
 # from django.shortcuts import get_object_or_404, render
-# from django.urls import reverse
+# from django.urls import reverse#
 from datetime import datetime
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
