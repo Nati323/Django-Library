abd9f9cc134cfed6e5abbf2764ac49b6942f196c
Nati <netanelzelzermn@gmail.com>
2023-01-05 20:11:19 +0200

    removed unused tailwind app

--- Library/settings.py
+++ Library/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'library_app',
-    'tailwind'
+    'library_app'
 ]
 
 MIDDLEWARE = [
